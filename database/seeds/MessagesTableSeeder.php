49fb8d64fbe9e73570e48219e9c36378f839ebbc
kenichi.hajake <hajake@astro-b.jp>
2020-08-20 06:00:17 +0000

    Procfile

---
+++ database/seeds/MessagesTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MessagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+        DB::table('messages')->insert([
+            'title' => 'test title 1',
+            'content' => 'test content 1'
+        ]);
+        DB::table('messages')->insert([
+            'title' => 'test title 2',
+            'content' => 'test content 2'
+        ]);
+        DB::table('messages')->insert([
+            'title' => 'test title 3',
+            'content' => 'test content 3'
+        ]);*/
+            for($i = 4; $i <= 30; $i++) {
+            DB::table('messages')->insert([
+                'title' => 'test title ' . $i,
+                'content' => 'test content ' . $i
+            ]);
+        }
+    }
+}
